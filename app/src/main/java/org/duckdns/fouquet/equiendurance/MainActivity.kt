09370bba0c44d3b06ad1129db0b88c23bb8d2461
ph.fouquet@gmail.com <ph.fouquet@gmail.com>
2024-04-29 17:44:09 +0200

    Add button

--- app/src/main/java/org/duckdns/fouquet/equiendurance/MainActivity.kt
+++ app/src/main/java/org/duckdns/fouquet/equiendurance/MainActivity.kt
@@ -6,6 +6,8 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
@@ -37,6 +39,13 @@ class MainActivity : AppCompatActivity() {
             override fun onProviderEnabled(provider: String) {}
             override fun onProviderDisabled(provider: String) {}
         }
+
+        findViewById<Button>(R.id.button_raz)
+            .setOnClickListener {
+                (locationList as ArrayList<Location>).clear()
+                (timeList as ArrayList<Long>).clear()
+            }
+
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
